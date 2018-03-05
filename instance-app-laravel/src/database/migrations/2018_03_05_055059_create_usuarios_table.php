46b60173bbedb92567025e9da9a3a40a67299aca
Marcos Silva Cunha <marcos21xx@gmail.com>
2018-03-05 02:55:54 -0300

    Model Usuário e sua tabela

--- instance-app-laravel/src/database/migrations/2018_03_05_055059_create_usuarios_table.php
+++ instance-app-laravel/src/database/migrations/2018_03_05_055059_create_usuarios_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUsuariosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('usuarios', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nome', 200);
+            $table->string('email', 100);
+            $table->string('senha', 50);
+            $table->date('datanasc');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('usuarios');
     }
 }
